e7902d01b71c38e1d7eb07912075f4fb785abc03
akaleans <31224906+akaleans@users.noreply.github.com>
2021-09-09 12:48:18 -0700

    first

---
+++ frontend/src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Component } from "react";
+import { render } from "react-dom";
+import HomePage from "./HomePage";
+
+export default function App(props) {
+    return (
+        <div className="center">
+            <HomePage />
+        </div>
+    );
+}
+
+const appDiv = document.getElementById("app");
+render(<App />, appDiv);
---
+++ spotify/credentials.py
@@ -0,0 +1,3 @@
+CLIENT_ID = "2a91dff460504affb95085dd23a015d2"
+CLIENT_SECRET = "c952f292daeb4c21b9b24311b055fcff"
+REDIRECT_URI = "http://127.0.0.1:8000/spotify/redirect"
